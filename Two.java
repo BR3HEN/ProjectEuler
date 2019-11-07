22438fc2109c635e9824f7a7eb20b30a85ada41a
Krzysiek <57046572+BR3HEN@users.noreply.github.com>
2019-11-07 15:16:36 +0100

    comments added

--- Two.java
+++ Two.java
@@ -1,23 +1,26 @@
 package OneTen;
-
+//By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 public class Two {
     public static void main(String[] arg){
 
-        long result = sumEvenFiboNumbers(4000000);
+        long result = sumEvenFiboNumbers(4000000);//wywołaj metodę
         System.out.println("Wynik sumy liczb parzystych ciągu to " + result);
 
     }
 
     static long sumEvenFiboNumbers(long n){
+        //zmienne początkowe
         long a = 0;
         long b = 1;
         long c = a + b;
         long sum = 0;
         long lastFibboNumber = 0;
+        
         while (c < n){
-            if(c % 2 == 0){
-                sum += c;
+            if(c % 2 == 0){//sprawdź czy parzysta liczba 
+                sum += c;//jeśli true to dodaj do sumy końcowej
             }
+            //następne liczby ciągu fibonacciego
             a = b;
             b = c;
             c = a + b;
