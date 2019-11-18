b5ad9f93daedef53b296d5d4620d1d3bbebec051
Krzysiek <57046572+BR3HEN@users.noreply.github.com>
2019-11-18 19:52:44 +0100

    first commit

---
+++ Sixteen.java
@@ -0,0 +1,22 @@
+package ElevenTwenty;
+
+import java.math.BigInteger;
+
+public class Sixteen {
+    public static void main(String[] args){
+
+        BigInteger base = new BigInteger(String.valueOf(2));
+        BigInteger veryBigNumber = base.pow(1000);
+        System.out.println(veryBigNumber);
+        String stringBigNumber = String.valueOf(veryBigNumber);
+
+        int lenght = stringBigNumber.length();
+        int sum = 0;
+
+        for (int i = 0; i < lenght; i++){
+            int digit = Character.getNumericValue(stringBigNumber.charAt(i));
+            sum += digit;
+        }
+        System.out.println(sum);
+    }
+}
