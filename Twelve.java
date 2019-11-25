48e8de9a402a26f28f82ffb00738af38c3e2b269
Krzysiek <57046572+BR3HEN@users.noreply.github.com>
2019-11-25 19:35:47 +0100

    second commit

--- Twelve.java
+++ Twelve.java
@@ -3,19 +3,19 @@ package ElevenTwenty;
 public class Twelve {
     public static void TriangleNum(){//liczy triangle numbers i odwołuje się do metody sprawdzającej ilość dzielników
         int triNum = 1;
-        for (int i = 2;; i++){
+        for (int i = 1;; i++){
+            triNum = ((i + 1) * i) / 2;
             if (divisors500(triNum)) {
                 System.out.println("Wynik to : " + triNum);
                 break;
             }
-            triNum += i;
         }
     }
 
     public static boolean divisors500(int triangleNum){
         int counter = 0;
-        for (int i = 1; i <= Math.sqrt(triangleNum); i++){//pierwiastek z triangle z uwagi na zbyt dużo obliczeń
-            if (triangleNum % i == 0) counter++;
+        for (int i = 1; i <= Math.sqrt(triangleNum); i++){//pierwiastek kw. z l. całk. prawie zawsze daje 1/2 ilość jej dzielników
+            if (triangleNum % i == 0) counter += 2;//za wyjątkiem idealnych pierw. kw tj. 4,9,16..
         }
         counter *= 2;//pomnóż licznik x2 ponieważ wcześniej dawał połowe wyniku
         if (counter > 500) return true;
