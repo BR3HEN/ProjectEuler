01cb42eb1c44e0e26870762323753dfb40d1c55d
Krzysiek <57046572+BR3HEN@users.noreply.github.com>
2019-12-05 12:37:59 +0100

    Update and rename Three.java to Euler3.java

--- Euler3.java
+++ Euler3.java
@@ -1,6 +1,6 @@
 package OneTen;
 
-public class Three {
+public class Euler3 {
     public static void main(String[] args){
 //What is the largest prime factor of the number 600851475143 ?
 //prime factory to liczba całkowita podzielna bez reszty oraz podzielna przez samą siebie.
